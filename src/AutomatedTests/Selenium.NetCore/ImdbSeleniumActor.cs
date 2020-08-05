82a26bb0be9aab9dd51bf9a10eb33bb06081687a
ambsenyestudi <adrifre7@hotmail.com>
2020-08-05 20:04:57 +0200

    Docs update and code tidding

--- src/AutomatedTests/Selenium.NetCore/ImdbSeleniumActor.cs
+++ src/AutomatedTests/Selenium.NetCore/ImdbSeleniumActor.cs
@@ -10,15 +10,21 @@ namespace Selenium.NetCore
     {
         private readonly bool isWait;
         public const string IMDB_URL = "https://www.imdb.com/";
-        public const string MOVIE_HEADER = "findHeader";
-        public const string SEARCH_INPUT = "suggestion-search";
+        public const string MOVIE_HEADER_CLASS = "findHeader";
+        public const string SEARCH_INPUT_ID = "suggestion-search";
+        public const string MORE_LINK_TEXT = "More title matches";
+        public const string MORE_LIST_CLASS = "findList";
         public By SearchElement { get; }
         public By MovieHeaderElment { get; }
+        public By MoreLikeThisElement { get; }
+        public By MoreLikeThisListElmeent { get; }
 
         public ImdbSeleniumActor(bool isWait = false)
         {
-            SearchElement = By.Id(SEARCH_INPUT);
-            MovieHeaderElment = By.ClassName("findHeader");
+            SearchElement = By.Id(SEARCH_INPUT_ID);
+            MovieHeaderElment = By.ClassName(MOVIE_HEADER_CLASS);
+            MoreLikeThisElement = By.LinkText(MORE_LINK_TEXT);
+            MoreLikeThisListElmeent = By.ClassName("findList");
             this.isWait = isWait;
         }
 
@@ -38,9 +44,9 @@ namespace Selenium.NetCore
             {
                 return false;
             }
-            var relatedMovies = FindClickableElement(By.LinkText("More title matches"));
+            var relatedMovies = FindClickableElement(MoreLikeThisElement);
             relatedMovies.Click();
-            var titleList = FindVisibleElement(By.ClassName("findList"));
+            var titleList = FindVisibleElement(MoreLikeThisListElmeent);
             return titleList != null;
         }
         private bool IsMovieFound(string title) =>
